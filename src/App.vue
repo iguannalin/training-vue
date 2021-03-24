debf1121c16e81ffe2c624816c3e2393edc0e897
Anna Lin <alin@mitre.org>
2021-03-24 15:33:39 -0700

    Init

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+
+<template>
+  <div>
+    <h1 class="text-4xl text-white bg-black">
+      {{name}}
+    </h1>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from "vue";
+
+  export default Vue.extend({
+    data: function() {
+      return {
+        name: 'Hello World!',
+      }
+    },
+  });
+</script>
+
+<style global>
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
