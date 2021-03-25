627ac2fd71edb42f727151f40e85bff068f78564
Anna Lin <alin@mitre.org>
2021-03-25 00:35:24 -0700

    Added flag child vue component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Flag msg="This is parent speaking"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Flag from './components/Flag.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Flag
   }
 }
 </script>
