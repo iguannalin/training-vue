e7fc822369a9305e4186f8d4bfd2d7c6b4985928
Anna Lin <alin@mitre.org>
2021-04-12 11:27:22 -0700

    Added dropdown for certificates, cleaned up UI

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div class="flex border-caldera-red border-2 rounded-3xl m-5 p-6 text-colors-white font-sans bg-caldera-grayish">
     <div class="flex justify-center flex-col w-full">
       <div>
-        <!--      TITLE-->
+        <!--  TITLE -->
         <div class="flex justify-between mt-2">
           <div class="flex flex-row items-center">
             <h2 class="text-left text-colors-white text-2xl">Training<span v-if="selectedCert">: {{
@@ -28,49 +28,58 @@
             </button>
           </div>
         </div>
-        <!--      END TITLE-->
-        <div class="flex justify-start items-center flex-wrap text-colors-white mt-3 mb-2">
-          <h3 class="text-lg mr-1">Certificates:</h3>
-          <div class="flex justify-start items-center flex-col hover:bg-caldera-red rounded"
-               v-for="(certificate, index) in certificateTypes" :key="index">
-            <button
-                class="flex flex-col justify-center items-center text-base text-colors-white p-1 ml-1 mr-1 font-bold w-24"
-                v-bind:class="{'font-bold': selectedCert === certificate}" v-bind:value="certificate"
-                v-on:click="(selectedCert === certificate) ? selectedCert = '' : selectedCert = certificate">
-            <span class="flex flex-col justify-center items-center w-4">
-                <span class="text-sm pb-1">{{ certificate }}</span>
-            </span>
-            </button>
+        <!--  END TITLE -->
+        <div class="flex flex-wrap flex-row">
+          <!--  CERTIFICATES  -->
+          <div class="flex justify-start items-start flex-wrap text-colors-white mt-3 mb-2 p-1 w-5/12">
+            <div class="flex flex-row justify-center">
+              <label for="certificate-select" class="text-lg mt-2 mr-1">Certificates:</label>
+              <div class="text-colors-black m-1">
+                <select id="certificate-select" class="p-2 h-auto rounded" v-model="selectedCert">
+                  <option disabled value="">Please choose a certificate</option>
+                  <option v-for="(certificate, index) in certificateTypes" :key="index">{{ certificate }}</option>
+                </select>
+              </div>
+            </div>
           </div>
-        </div>
-
-        <div v-if="selectedCert" class="flex justify-start items-center flex-wrap text-colors-white mt-3 mb-2">
-          <h3 class="text-lg mr-1">Badges:</h3>
-          <div class="flex justify-center items-center flex-col hover:bg-caldera-red rounded"
-               v-for="(badge, index) in badgeList" :key="index">
-            <button
-                class="flex flex-col justify-center items-center text-base text-colors-white mt-3 p-1 ml-1 mr-1 w-20"
-                v-bind:class="{'font-bold': selectedBadge === badge}" v-bind:value="selectedBadge"
-                v-on:click="(selectedBadge === badge) ? selectedBadge = '' : selectedBadge = badge">
-            <span class="flex flex-col justify-center items-center">
-              <span class="z-0 absolute w-14 fill-current">
-               <svg xmlns="http://www.w3.org/2000/svg" fill="current" viewBox="0 0 24 24" stroke="currentColor"><path
-                   stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                   d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"/></svg>
-              </span>
-              <img v-bind:alt="badge.name" class="w-6 z-10" v-bind:src="badge.iconSrc"/>
-            </span>
-              <span class="hover:bg-caldera-red rounded pl-1 pr-1 text-xs mt-4">{{ badge.name }}</span>
-            </button>
+          <!--  END CERTIFICATES  -->
+          <!--  BADGES  -->
+          <div v-if="selectedCert" class="flex justify-start items-center flex-wrap text-colors-white mt-3 mb-2 p-1 w-7/12">
+            <div class="flex flex-row flex-wrap">
+              <div class="flex justify-end items-start">
+                <h3 class="text-lg mt-2 mr-1">Badges:</h3>
+              </div>
+              <div class="flex flex-row flex-wrap w-10/12 items-start">
+                <div class="flex justify-center items-center flex-col hover:bg-caldera-red rounded"
+                     v-for="(badge, index) in badgeList" :key="index">
+                  <button
+                      class="flex flex-col justify-center items-center text-base text-colors-white mt-3 p-1 ml-1 mr-1 w-20"
+                      v-bind:class="{'font-bold': selectedBadge === badge}" v-bind:value="selectedBadge"
+                      v-on:click="(selectedBadge === badge) ? selectedBadge = '' : selectedBadge = badge">
+                    <span class="flex flex-col justify-center items-center">
+                      <span class="z-0 absolute w-14 fill-current">
+                       <svg xmlns="http://www.w3.org/2000/svg" fill="current" viewBox="0 0 24 24" stroke="currentColor"><path
+                           stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                           d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"/></svg>
+                      </span>
+                      <img v-bind:alt="badge.name" class="w-6 z-10" v-bind:src="badge.iconSrc"/>
+                    </span>
+                    <span class="hover:bg-caldera-red rounded pl-1 pr-1 text-xs mt-4">{{ badge.name }}</span>
+                  </button>
+                </div>
+              </div>
+            </div>
           </div>
+          <!--  END BADGES  -->
         </div>
-
+        <!--  FLAGS  -->
         <div v-if="selectedCert" class="flex mt-1 flex-row flex-wrap justify-start">
           <div class="lg:w-3/12 md:w-4/12 sm:w-full p-2 box-border" v-for="(flag, index) in visibleFlagList"
                :key="index">
             <Flag :flag="flag"></Flag>
           </div>
         </div>
+        <!--  END FLAGS  -->
       </div>
     </div>
   </div>
@@ -92,20 +101,20 @@ export default {
       badgeList: [],
       flagList: [],
       visibleFlagList: [],
-      certificateTypes: ["User Certificate", "Blue Certificate"]
+      certificateTypes: ['User Certificate', 'Blue Certificate']
     }
   },
   watch: {
     selectedCert: function () {
       if (this.selectedCert) {
-        console.log('SELECTED', this.selectedCert);
+        console.log('SELECTED CERT', this.selectedCert);
         // TODO remove this dev version
         if (!this.getTraining()) this.getFlags(data);
       } else this.resetData();
     },
     selectedBadge: function () {
       if (this.selectedBadge) {
-        console.log('SELECTED', this.selectedBadge);
+        console.log('SELECTED BADGE', this.selectedBadge);
         this.visibleFlagList = this.flagList.filter(flag => flag.badge_name === this.selectedBadge.name);
       } else this.visibleFlagList = this.flagList;
     }
